9ca66c4bdbb16b0f0c70f488e90a6a4f0f6754fe
ChangHangeol <hgchang1@naver.com>
2025-07-14 00:19:06 +0900

    250713

--- quantylab/networks/networks_keras.py
+++ quantylab/networks/networks_keras.py
@@ -17,3 +17,38 @@ elif os.environ['KERAS_BACKEND'] == 'plaidml.keras.backend':
     from keras.layers import Input, Dense, LSTM, Conv1D, \
         BatchNormalization, Dropout, MaxPooling1D, Flatten
     from keras.optimizers import SGD
+# 임포트 끝
+
+class Network:
+    lock = threading.Lock()
+
+    def __init__(self, input_dim=0, output_dim=0, lr=0.001,
+                 shared_network=None, activation='sigmoid', loss='mse'):
+        
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.lr = lr
+        self.shared_network = shared_network
+        self.activation = activation
+        self.loss = loss
+        self.model = None
+
+    def predict(self, sample):
+        with self.lock:
+            pred = self.model.predict_on_batch(sample).flatten()
+            return pred
+
+    def train_on_batch(self, x, y):
+        loss = 0.
+        with self.lock:
+            history = self.model.fit(x, y, epochs=1, verbose=False)
+            loss += np.sum(history.history['loss'])
+        return loss
+    
+    def save_model(self, model_path):
+        with model_path is not None and self.model is not None:
+            self.model.save_weights(model_path, overwrite=True)
+
+    def load_model(self, model_path):
+        if model_path is not None:
+            self.model.load_weights(model_path)
